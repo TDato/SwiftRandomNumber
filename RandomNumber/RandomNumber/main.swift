3eed1b5074ff6720246509279b86eded23562b2d
Tommy Dato <tom.dato@gmail.com>
2018-11-29 20:52:56 -0600

    Simple Guessing Game
    Game working with number range 0-100

---
+++ RandomNumber/RandomNumber/main.swift
@@ -0,0 +1,65 @@
+//
+//  main.swift
+//  RandomNumber
+//
+//  Created by Thomas Dato on 11/29/18.
+//  Copyright © 2018 Tommy Dato. All rights reserved.
+//
+
+import Foundation
+
+var randomNumber = 1
+var continueGuessing = true
+var keepPlaying = true
+var input = ""
+
+while keepPlaying
+{
+    randomNumber = Int(arc4random_uniform(101)) // get a random number between 0 and 100
+    print("The random number to guess is: \(randomNumber)")
+    
+    while continueGuessing
+    {
+        print("Pick a number between 0 and 100.")
+        
+        input = NSString(data: FileHandle.standardInput.availableData, encoding:String.Encoding.utf8.rawValue)! as String // Get Keyboard input
+        input = input.replacingOccurrences(of: "\n", with: "", options: NSString.CompareOptions.literal, range:nil) // strips off the \n
+        
+        if let userGuess = Int(input)
+        {
+            if userGuess == randomNumber
+            {
+                // input correct
+                continueGuessing = false
+                print("That's the correct number!")
+            }
+            else if userGuess > randomNumber
+            {
+                // input too high
+                print("Your guess is too high!")
+            }
+            else
+            {
+                // input too low
+                print("Your guess is too low!")
+            }
+        }
+        else
+        {
+            // invalid input
+            print("Invalid guess, please try again.")
+        }
+    }
+    
+    // get input for keepPlaying
+    print("Would you like to play again? Y or N")
+    input = NSString(data: FileHandle.standardInput.availableData, encoding:String.Encoding.utf8.rawValue)! as String
+    input = input.replacingOccurrences(of: "\n", with: "", options: NSString.CompareOptions.literal, range: nil)
+    
+    if input == "N" || input == "n"
+    {
+        keepPlaying = false
+    }
+    continueGuessing = true
+}
+
